d454b93b95a7ad2d4822e0a31022dacf628e3d70
Dmitriy B <kupperfeet@gmail.com>
2022-08-31 17:38:41 +0300

    fibo recursiv

---
+++ task4/Program.cs
@@ -0,0 +1,13 @@
+﻿// f(1) = 1
+// f(2) = 1
+
+
+int Fibonacci (int n)
+{
+    if (n == 1 || n ==2) return 1;
+    else return Fibonacci(n-1) + Fibonacci (n-2);
+}
+for (int i =1; i < 10; i++)
+{
+    Console.WriteLine(Fibonacci(i));
+}
