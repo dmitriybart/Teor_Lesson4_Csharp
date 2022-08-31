6e094a7587f9e968ceb11d6696c934e99f20cb70
Dmitriy B <kupperfeet@gmail.com>
2022-08-31 17:15:37 +0300

    first commit2

---
+++ task2/Program.cs
@@ -0,0 +1,15 @@
+﻿void PrintImage (int[,] image)
+{
+    var random = new Random();
+    for (int i = 0; i < image.GetLength(0); i++)
+    {
+        for (int j = 0; j < image.GetLength(1); j++)
+        {
+            if (image[i,j] == 0) Console.Write($"");
+            else Console.Write($"+");
+        }
+    }
+    Console.WriteLine();
+}
+
+PrintImage()
